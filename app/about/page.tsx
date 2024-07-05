10f4a9184e5d8d6be5f6d657d471866dc78e50b0
HagisawaYuki <hagisawa7110@gmail.com>
2024-07-05 11:50:07 +0900

    ２回目コミット

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+export default function About() {
+    return (
+      <div>
+          <h2 className="font-bold" style={{fontSize: `20px`, margin: `3%`}}>
+            about画面
+          </h2>
+      </div>
+    );
+}
---
+++ app/content/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+export default function Content() {
+    return (
+      <div>
+          <h2 className="font-bold" style={{fontSize: `20px`, margin: `3%`}}>
+            content画面
+          </h2>
+      </div>
+    );
+}
